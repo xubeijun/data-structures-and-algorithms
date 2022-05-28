e41208c32e72573fee25a021663437a3254e50d0
frank.wong <frank.wong@ruoshitech.com>
2022-05-28 18:25:16 +0800

    mark todo root-of-avl-tree

--- README.md
+++ README.md
@@ -46,6 +46,7 @@ Sequence | Problems | Solution | Remarks
 　　5.2 | List all the leaves | [list-leaves.c](https://github.com/xubeijun/data-structures-and-algorithms/tree/main/problems/tree/list-leaves.c) | [todo](list-leaves.c)
 　　5.3 | Tree traversals again - post order traversal | [tree-traversals-again.c](https://github.com/xubeijun/data-structures-and-algorithms/tree/main/problems/tree/tree-traversals-again.c) | [todo](tree-traversals-again.c)
 　　5.4 | The same binary search tree | [same-binary-search-tree.c](https://github.com/xubeijun/data-structures-and-algorithms/tree/main/problems/tree/same-binary-search-tree.c) | [todo](same-binary-search-tree.c)
+　　5.5 | Root of AVL Tree | [root-of-avl-tree.c](https://github.com/xubeijun/data-structures-and-algorithms/tree/main/problems/tree/root-of-avl-tree.c) | [todo](root-of-avl-tree.c)
 
 
 ## Follow me
---
+++ problems/tree/root-of-avl-tree.c
@@ -0,0 +1,34 @@
+/*
+
+高度平衡树的根
+
+04-树5 Root of AVL Tree
+
+An AVL tree is a self-balancing binary search tree. In an AVL tree, the heights of the two child subtrees of any node differ by at most one; if at any time they differ by more than one, rebalancing is done to restore this property. Figures 1-4 illustrate the rotation rules.
+
+Now given a sequence of insertions, you are supposed to tell the root of the resulting AVL tree.
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line contains a positive integer N (≤20) which is the total number of keys to be inserted. Then N distinct integer keys are given in the next line. All the numbers in a line are separated by a space.
+
+Output Specification:
+
+For each test case, print the root of the resulting AVL tree in one line.
+
+Sample Input 1:
+
+5
+88 70 61 96 120
+Sample Output 1:
+70
+Sample Input 2:
+7
+88 70 61 96 120 90 65
+
+Sample Output 2:
+
+88
+
+[04-树5 Root of AVL Tree - JIANRU啊](https://blog.nowcoder.net/n/ca726fe484f04ea5bb536a74f6d78936)
+*/
